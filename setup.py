dc263bc33abbbf904a754a97f7d30e3ed26bb29c
bookisLY <37164148+bookisLY@users.noreply.github.com>
2019-11-01 17:38:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Lysort',
       version='0.1.0',
       description='There have Six sort functions to sort data',
-      url = 'https://github.com/bookisLY/Lysort',
+      url = 'https://github.com/bookisLY/',
       author='bookist123',
       author_email='421736951@qq.com',
       license='MIT',
