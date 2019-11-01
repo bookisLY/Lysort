eb186e4c84f22f8e280d5b38745ac16e88542b66
bookisLY <37164148+bookisLY@users.noreply.github.com>
2019-11-01 14:39:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='Lysort',
       version='0.1.0',
       description='There have Six sort functions to sort data
       and provide a func to make a random list or iterable object'
-      url = ''
+      url = 'https://github.com/bookisLY/Lysort'
       author='bookist123',
       author_email='421736951@qq.com',
       license='MIT',
